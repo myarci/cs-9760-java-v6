af7c497809daed68c1ded18cf5e531e4be78fa78
Metin Yarci <metin.yarci@aurea.com>
2019-08-16 06:55:24 +0300

    revision1

---
+++ directory1/directory2/AParentClass.java
@@ -0,0 +1,17 @@
+package directory1.directory2;
+
+public class AParentClass {
+
+    public void myParentMethod() {
+    	int myVar = 0;
+    }
+
+    public class ALongClass {
+
+        public void myChildMethod() {
+    		int myVar = 1;
+        }
+
+    }
+
+}
